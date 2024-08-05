bc510028ca9e94f2fd25bce1c9f774bc490bf3d3
Matheus Santos <somathsant@gmail.com>
2024-08-04 22:10:27 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { preprocess, z } from 'zod';
+
+const app = fastify();
+
+const prisma = new PrismaClient();
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany();
+
+  return { users };
+});
+
+app.post('/users', async (req, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
+
+  const { name, email } = createUserSchema.parse(req.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return reply.status(201).send();
+});
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running');
+  });
